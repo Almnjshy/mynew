5dd2c23b9c896e055f9d5c0bcbc2aa1823494b34
Almnjshy <115098081+Almnjshy@users.noreply.github.com>
2026-07-06 17:56:13 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css'
 import { useGameStore } from './store/gameStore'
 import { useAndroidBackButton } from './hooks/useAndroidBackButton'
 import ErrorBoundary from './components/ErrorBoundary'
-import ExitConfirmation from './components/ExitConfirmation'
 import ScreenTransition from './components/ScreenTransition'
 import TitleScreen from './screens/TitleScreen'
 import MainMenu from './screens/MainMenu'
@@ -18,6 +17,12 @@ import ProfileScreen from './screens/ProfileScreen'
 import LeaderboardScreen from './screens/LeaderboardScreen'
 import WifiGameScreen from './screens/WifiGameScreen'
 import OnlineGameScreen from './screens/OnlineGameScreen'
+// Tournament Screens
+import TournamentMenuScreen from './screens/TournamentMenuScreen'
+import TournamentCreateScreen from './screens/TournamentCreateScreen'
+import TournamentBracketScreen from './screens/TournamentBracketScreen'
+import TournamentGameScreen from './screens/TournamentGameScreen'
+import TournamentHistoryScreen from './screens/TournamentHistoryScreen'
 
 function App() {
   const { screen } = useGameStore()
@@ -61,6 +66,12 @@ function App() {
       case 'leaderboard': return <LeaderboardScreen />
       case 'wifiGame': return <WifiGameScreen />
       case 'onlineGame': return <OnlineGameScreen />
+      // Tournament screens
+      case 'tournamentMenu': return <TournamentMenuScreen />
+      case 'tournamentCreate': return <TournamentCreateScreen />
+      case 'tournamentBracket': return <TournamentBracketScreen />
+      case 'tournamentGame': return <TournamentGameScreen />
+      case 'tournamentHistory': return <TournamentHistoryScreen />
       default: return <TitleScreen />
     }
   }
@@ -72,7 +83,6 @@ function App() {
           {renderScreen()}
         </ScreenTransition>
       </div>
-      <ExitConfirmation />
     </ErrorBoundary>
   )
 }
